475d90e78907935ac3e9712fbad6f67c21f6a89b
parix1999 <parisottoedoardo@gmail.com>
2021-09-09 18:58:45 +0200

    base per il databse

---
+++ app/Movie.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    // Qui dentro è come se avessi tutte le proprietà del mio database
+    // come se ci fosse scritto:
+    /*
+    public $id;
+    public $title;
+    public $timing;
+    public $description;
+    public $year;
+    public $cover;
+
+    Ci sono tutte le proprietà scritte nelle tabelle
+    */
+    
+
+
+}
